74a72d3b56f4d5d23685bc08a55d4e5bf8a45644
manuelashy <manuelashy@gmail.com>
2025-02-27 06:00:55 +0000

    secod commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+document.querySelectorAll(".animate-on-scroll").forEach((element) => {
+  observer.observe(element);
+});
